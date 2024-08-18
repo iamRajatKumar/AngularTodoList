0368903320679a129c8417a9afdfc58cc0739cf5
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-08-19 00:20:37 +0530

    publish

--- src/app/myComponents/todos/todos.component.ts
+++ src/app/myComponents/todos/todos.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
+import { TodoItemComponent } from "../todo-item/todo-item.component";
+import { AddTodoComponent } from "../add-todo/add-todo.component";
+
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TodoItemComponent, AddTodoComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
 
+  todos:Todo[];
+  constructor() { 
+    this.todos = [
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title 2",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 3,
+        title: "This is title3",
+        desc: "Description",
+        active: true
+      }
+    ]
+  }
+
+  deleteTodo(todo:Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1)
+  }
 }
